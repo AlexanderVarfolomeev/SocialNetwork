6049c56e7e6b2e67bcf94c0ef85ccacd83e8a6a8
AlexanderVarfolomeev <alexander.varfolomei@gmail.com>
2023-04-09 17:25:04 +0300

    Add config to worker

---
+++ src/Systems/SocialNetwork.Worker/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using SocialNetwork.EmailService;
+using SocialNetwork.RabbitMq;
+using SocialNetwork.Worker.TaskExecutor;
+
+namespace SocialNetwork.Worker;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+    {
+        services
+            .AddRabbitMq()            
+            .AddEmailService()
+            ;
+
+        services.AddSingleton<ITaskExecutor, TaskExecutor.TaskExecutor>();
+
+        return services;
+    }
+}
+
+
